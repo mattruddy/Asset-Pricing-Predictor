20dfdce330ff7fc885f4bbc9e508e445e3548f07
Matt <mjruddy94@gmail.com>
2017-12-01 19:35:03 -0500

    add percent change

--- stocks.py
+++ stocks.py
@@ -8,10 +8,25 @@ path = "/Users/mattruddy/Desktop/intraQuarter"
 def Key_Stats(gather="Total Debt/Equity (mrq)"):
     statspath = path+'/_KeyStats'
     stock_list = [x[0] for x in os.walk(statspath)]
-    df = pd.DataFrame(columns = ['Date','Unix','Ticker','DE Ratio'])
-    for each_dir in stock_list[1:4]:
+    df = pd.DataFrame(columns = ['Date',
+                                'Unix',
+                                'Ticker',
+                                'DE Ratio',
+                                'Price',
+                                'stock_p_change',
+                                'SP500',
+                                'sp500_p_change'])
+
+    sp500_df = pd.DataFrame.from_csv("HistoricalPrices.csv")
+
+    ticker_list = []
+
+    for each_dir in stock_list[1:25]:
         each_file = os.listdir(each_dir)
         ticker = each_dir.split("/")[-1]
+        ticker_list.append(ticker)
+        starting_stock_value = False
+        starting_sp500_value = False
         if len(each_file) > 0:
             for file in each_file:
                 date_stamp = datetime.strptime(file, '%Y%m%d%H%M%S.html')
@@ -20,10 +35,35 @@ def Key_Stats(gather="Total Debt/Equity (mrq)"):
                 source = open(full_file_path,'r').read()
                 try:
                     value = float(source.split(gather+':</td><td class="yfnc_tabledata1">')[1].split('</td>')[0])
-                    df = df.append({'Date':date_stamp,'Unix':unix_time,'Ticker':ticker,'DE Ratio':value,}, ignore_index = True)
+                    stock_price = float(source.split('</small><big><b>')[1].split('</b></big>')[0])
+
+                    try:
+                        sp500_date = datetime.fromtimestamp(unix_time - 259200).strftime('%Y-%m-%d')
+                        row = sp500_df[(sp500_df.index == sp500_date)]
+                        sp500_value = float(row[" Close"])
+                    except:
+                        sp500_date = datetime.fromtimestamp(unix_time).strftime('%Y-%m-%d')
+                        row = sp500_df[(sp500_df.index == sp500_date)]
+                        sp500_value = float(row[" Close"])
+
+                    if not starting_stock_value:
+                        starting_stock_value = stock_price
+                    if not starting_sp500_value:
+                        starting_sp500_value = sp500_value
+
+                    stock_p_change = ((stock_price - starting_stock_value) / starting_stock_value) * 100
+                    sp500_p_change = ((sp500_value - starting_sp500_value) / starting_sp500_value) * 100
+
+                    df = df.append({'Date':date_stamp,
+                    'Unix':unix_time,
+                    'Ticker':ticker,
+                    'DE Ratio':value,
+                    'Price':stock_price,
+                    'stock_p_change':stock_p_change,
+                    'SP500':sp500_value,
+                    'sp500_p_change':sp500_p_change,},ignore_index = True)
                 except Exception as e:
                     pass
-            time.sleep(15)
     save = gather.replace(' ','').replace('(','').replace(')','').replace('/','')+('.csv')
     print(save)
     df.to_csv(save)
